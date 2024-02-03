95d4affaee1053af71b7bbfd031cbd15477574b3
Kel Cuprum <me@kelcuprum.ru>
2024-02-03 08:47:37 +0300

    Update files - Edit ConfigScreen - Update AlinLib 1.0.1a -> 1.1.0-beta.1

--- src/main/java/ru/kelcuprum/ruthenium/screen/ConfigScreen.java
+++ src/main/java/ru/kelcuprum/ruthenium/screen/ConfigScreen.java
@@ -4,9 +4,10 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
-import ru.kelcuprum.alinlib.gui.components.buttons.Button;
-import ru.kelcuprum.alinlib.gui.components.buttons.ButtonBoolean;
-import ru.kelcuprum.alinlib.gui.components.sliders.SliderInteger;
+import ru.kelcuprum.alinlib.gui.InterfaceUtils;
+import ru.kelcuprum.alinlib.gui.components.buttons.ButtonConfigBoolean;
+import ru.kelcuprum.alinlib.gui.components.buttons.base.Button;
+import ru.kelcuprum.alinlib.gui.components.sliders.SliderConfigInteger;
 import ru.kelcuprum.alinlib.gui.components.text.TextBox;
 import ru.kelcuprum.ruthenium.Client;
 
@@ -25,28 +26,22 @@ public class ConfigScreen extends Screen {
         this.parent = parent;
     }
 
-    public void tick() {
-        super.tick();
-    }
-
     public void init() {
         this.initButtonsCategory();
     }
 
     private void initButtonsCategory() {
-        Objects.requireNonNull(this.font);
         addRenderableWidget(new TextBox(0, 15, this.width, 9, this.title, true));
         int x = width/2;
         int size = 180;
-        addRenderableWidget(new ButtonBoolean(x-90, 40, size, 20, Client.config, "AFK_FPS_ENABLE", true, afkFPSEnableText));
-        addRenderableWidget(new SliderInteger(x-90, 65, size, 20, Client.config, "AFK_FPS", 10,5, 60, afkFPSText));
+        addRenderableWidget(new ButtonConfigBoolean(x-90, 40, size, 20, Client.config, "AFK_FPS_ENABLE", true, afkFPSEnableText));
+        addRenderableWidget(new SliderConfigInteger(x-90, 65, size, 20, Client.config, "AFK_FPS", 10,5, 60, afkFPSText));
 
-        addRenderableWidget(new ButtonBoolean(x-90, 90, size, 20, Client.config, "AFK_DISTANCE_ENABLE", true, afkDistanceEnableText));
-        addRenderableWidget(new SliderInteger(x-90, 115, size, 20, Client.config, "AFK_DISTANCE", 2,2, 12, afkDistanceText));
+        addRenderableWidget(new ButtonConfigBoolean(x-90, 90, size, 20, Client.config, "AFK_DISTANCE_ENABLE", true, afkDistanceEnableText));
+        addRenderableWidget(new SliderConfigInteger(x-90, 115, size, 20, Client.config, "AFK_DISTANCE", 2,2, 12, afkDistanceText));
 
-        addRenderableWidget(new Button(x-90, height-30, size, 20, 0xFFff006e, CommonComponents.GUI_BACK, (s) -> {
-            assert this.minecraft != null;
-            this.minecraft.setScreen(this.parent);
+        addRenderableWidget(new Button(x-90, height-30, size, 20, InterfaceUtils.DesignType.ALINA, 0xFFff006e, CommonComponents.GUI_BACK, (s) -> {
+            onClose();
         }));
     }
 
@@ -55,6 +50,11 @@ public class ConfigScreen extends Screen {
         int x = width/2;
         guiGraphics.fill(x-100, 0, x+100, height, 0x7F000000);
         super.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
+    }
+    @Override
+    public void onClose(){
+        assert this.minecraft != null;
+        this.minecraft.setScreen(this.parent);
     }
 }
 
